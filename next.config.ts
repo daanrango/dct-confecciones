2c4402b5b059e516a526b0909b538f518b8e56c5
David Anrango <david.anrango@grupobusiness.it>
2026-03-06 18:20:52 -0500

    v1.2: Update next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  basePath: "/dct-confecciones",
 };
 
 export default nextConfig;
